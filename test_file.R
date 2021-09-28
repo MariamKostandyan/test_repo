01bc7b431c98e31e6c4d08582511016f5d8ff154
Mariam Kostandyan <kostandyan.m@gmail.com>
2021-09-28 15:33:00 +0200

    Data wrangling

--- test_file.R
+++ test_file.R
@@ -35,3 +35,37 @@ df <- read.csv("report_Value___Behaviour_rating_Edited.csv", header = TRUE, fill
                strip.white = FALSE, blank.lines.skip = TRUE,
                comment.char = "#",
                allowEscapes = FALSE, flush = FALSE)
+
+####---------------preparing the data set---------------####
+unique(df$Competency.Official.Rating)
+unique(df$Competency.Self.Rating)
+
+df1 <- df #create a duplicate of the data set
+
+#rename the ratings' values
+df1$Competency.Self.Rating <- revalue(df1$Competency.Self.Rating, c("Unrated"="unrated"))
+df1[df1 == ""] <- NA     
+df1$Competency.Self.Rating[is.na(df1$Competency.Self.Rating)] <- "manager rated, employee did not rate"
+df1$Competency.Self.Rating <- revalue(df1$Competency.Self.Rating, c("1"="development area"))
+df1$Competency.Self.Rating <- revalue(df1$Competency.Self.Rating, c("2"="demonstrates the value"))
+df1$Competency.Self.Rating <- revalue(df1$Competency.Self.Rating, c("3"="role model"))
+df1$Competency.Self.Rating <- revalue(df1$Competency.Self.Rating, c("0"="unable to rate"))
+
+df1$Competency.Official.Rating <- revalue(df1$Competency.Official.Rating, c("Unrated"="unrated"))
+df1$Competency.Official.Rating <- revalue(df1$Competency.Official.Rating, c("1"="development area"))
+df1$Competency.Official.Rating <- revalue(df1$Competency.Official.Rating, c("2"="demonstrates the value"))
+df1$Competency.Official.Rating <- revalue(df1$Competency.Official.Rating, c("3"="role model"))
+df1$Competency.Official.Rating <- revalue(df1$Competency.Official.Rating, c("0"="unable to rate"))
+
+unique(df1$Competency.Official.Rating)
+unique(df1$Competency.Self.Rating)
+
+# delete unable to rate, unrated, and manager rated, employee did not rate 
+df1 <- subset(df1, !Competency.Official.Rating %in% c("unable to rate", "unrated") &
+                !Competency.Self.Rating %in% c("unable to rate", "unrated", "manager rated, employee did not rate"))
+
+
+# subset two separate data sets about Values & Behavior 
+df_behav <- subset(df1, Section.Name == "Behaviours that Make A Difference")
+df_val <- subset(df1, Section.Name == "Values")
+
